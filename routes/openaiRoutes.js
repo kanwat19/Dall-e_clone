8c06a44bd795206a38d59b125c3a3766f8370704
kanwat19 <92249108+kanwat19@users.noreply.github.com>
2023-07-16 21:26:31 +0530

    Add files via upload

---
+++ routes/openaiRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { generateImage } = require('../controllers/openaiControllers');
+const router = express.Router();
+
+
+
+
+router.post('/generateimage', generateImage);
+module.exports = router;
